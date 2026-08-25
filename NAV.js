bd51731f31dd855a5b850d8941efd2a01f5dd35a
retropixel101 <retropixel404@gmail.com>
2026-08-24 23:41:04 -0400

    Create NAV.js

---
+++ NAV.js
@@ -0,0 +1,10 @@
+const SGS_ROUTES = {
+    home: "index.html",
+    games: "games.html",
+
+    utilities: "unfin.html",
+    browser: "unfin.html",
+    music: "unfin.html",
+    forums: "unfin.html",
+    settings: "unfin.html"
+};
